af7d9e16fcf1b462261e4393a73b4c160b6b8999
Colleen Stewart <collstewart30@gmail.com>
2022-07-22 12:31:28 -0400

    created classes, initializers, method framework

---
+++ battlefield.py
@@ -0,0 +1,22 @@
+from robot import Robot
+from dinosaur import Dinosaur
+
+
+class Battlefield:
+
+    def __init__(self):
+        self.Robot = ''
+        self.Dinosaur = ''
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
+
---
+++ robot.py
@@ -0,0 +1,16 @@
+from weapon import Weapon
+
+
+class Robot:
+
+    def __init__(self, name):
+        self.name = name
+        self.health = 0
+        self.active_weapon = Weapon()
+
+    def attack(self, dinosaur):
+        pass
+
+
+
+    
